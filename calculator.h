02104eabad092e5e293ce76fa3f994665b6dc55b
Quanti994 <62599567+Quanti994@users.noreply.github.com>
2020-03-31 15:34:30 +0200

    Create calculator.h

---
+++ calculator.h
@@ -0,0 +1,27 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+#include <stack>
+#include <iostream>
+
+enum OPERATIONS{ADD=1,SUB=2,MULTI=3,DIVI=4,EXIT=5};
+
+class calculator
+{
+    std::stack<double>numbers;
+    OPERATIONS operation;
+    bool errorFlag;
+    public:
+        calculator();
+        ~calculator();
+        double calculate();
+        void enterNumber(std::istream& input);
+        void enterOperation(std::istream&input);
+    private:
+        double addNumbers();
+        double subNumbers();
+        double multiNumbers();
+        double diviNumbers();
+        void exitCalculator();
+};
+
+#endif // CALCULATOR_H
