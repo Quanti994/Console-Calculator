ebce0dd51e850182fe0fe14d90edef1505ea28c9
Quanti994 <62599567+Quanti994@users.noreply.github.com>
2020-04-01 14:14:06 +0200

    Update calculator.h

--- calculator.h
+++ calculator.h
@@ -2,26 +2,31 @@
 #define CALCULATOR_H
 #include <stack>
 #include <iostream>
+#include <vector>
 
 enum OPERATIONS{ADD=1,SUB=2,MULTI=3,DIVI=4,EXIT=5};
+enum STATUS{START=1,CHECK_OPERATION=2,INTRODUCTION_NEXT_NUMBER=3};
 
 class calculator
 {
-    std::stack<double>numbers;
-    OPERATIONS operation;
-    bool errorFlag;
+       std::stack<double>numbers;
+       std::vector<std::string>calculatorBufor;
+       OPERATIONS operation;
+       STATUS calculatorStatus;
     public:
         calculator();
         ~calculator();
-        double calculate();
         void enterNumber(double number);
         void enterOperation(size_t numberOperation);
+        STATUS checkStatusCalculator();
+        void calculate();
+        friend std::ostream& operator<<(std::ostream& output,calculator &calc);
     private:
         double addNumbers();
         double subNumbers();
         double multiNumbers();
         double diviNumbers();
-        void exitCalculator();
+        void resetBufor();
 };
 
 #endif // CALCULATOR_H
