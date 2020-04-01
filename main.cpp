680bb182b841f969fe1b66a7f9c5a61537e92ac6
Quanti994 <62599567+Quanti994@users.noreply.github.com>
2020-04-01 14:13:15 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,49 +1,61 @@
 #include <iostream>
 #include "calculator.h"
+#include "console.h"
 
 int main()
 {
     calculator calc;
 
-    std::cout<<"KALKULATOR\n";
-    std::cout<<"=================\n";
-    std::cout<<"1.Dodawanie\n";
-    std::cout<<"2.Odejmowanie\n";
-    std::cout<<"3.Mnozenie\n";
-    std::cout<<"4.Dzielenie\n";
-    std::cout<<"5.WYJSCIE\n";
-    std::cout<<"=================\n";
+    for(;;){
 
-    double number;
-    do{
-    std::cin.clear();
-    std::cin.sync();
-    std::cout<<"Podaj pierwsza liczbe: ";
-    std::cin >> number;
-    if(std::cin.fail()) std::cout<<"BLAD TRESCI!"<<std::endl;
-    }while(std::cin.fail());
-    calc.enterNumber(number);
+        clearConsole();
 
-    size_t numberOperation;
-    do{
-    std::cin.clear();
-    std::cin.sync();
-    std::cout<<"Wybierz operacje: ";
-    std::cin >> numberOperation;
-    if(std::cin.fail()) std::cout<<"BLAD TRESCI!"<<std::endl;
-    }while(std::cin.fail());
-    calc.enterOperation(numberOperation);
+        std::cout<<"KALKULATOR\n";
+        std::cout<<"=================\n";
+        std::cout<<"1.Dodawanie\n";
+        std::cout<<"2.Odejmowanie\n";
+        std::cout<<"3.Mnozenie\n";
+        std::cout<<"4.Dzielenie\n";
+        std::cout<<"5.WYJSCIE\n";
+        std::cout<<"=================\n";
+        std::cout<<"WYNIK = "<<calc;
+        std::cout<<"=================\n";
 
-    do{
-    std::cin.clear();
-    std::cin.sync();
-    std::cout<<"Podaj druga liczbe: ";
-    std::cin >> number;
-    if(std::cin.fail()) std::cout<<"BLAD TRESCI!"<<std::endl;
-    }while(std::cin.fail());
-    calc.enterNumber(number);
-
-    std::cout<<"WYNIK = "<<calc.calculate();
+        double number;
+        size_t numberOperation;
+        if(calc.checkStatusCalculator()==START)
+        {
+           do{
+           std::cin.clear();
+           std::cin.sync();
+           std::cout<<"Podaj pierwsza liczbe: ";
+           std::cin >> number;
+           if(std::cin.fail()) std::cout<<"BLAD TRESCI!"<<std::endl;
+         }while(std::cin.fail());
+         calc.enterNumber(number);
+         } else if(calc.checkStatusCalculator()==CHECK_OPERATION)
+         {
+        do{
+        std::cin.clear();
+        std::cin.sync();
+        std::cout<<"Wybierz operacje: ";
+        std::cin >> numberOperation;
+        if(std::cin.fail()) std::cout<<"BLAD TRESCI!"<<std::endl;
+        }while(std::cin.fail());
+        calc.enterOperation(numberOperation);
+        } else if(calc.checkStatusCalculator()==INTRODUCTION_NEXT_NUMBER)
+        {
+        do{
+        std::cin.clear();
+        std::cin.sync();
+        std::cout<<"Podaj druga liczbe: ";
+        std::cin >> number;
+        if(std::cin.fail()) std::cout<<"BLAD TRESCI!"<<std::endl;
+        }while(std::cin.fail());
+        calc.enterNumber(number);
+        calc.calculate();
+        }
+    }
 
     return 0;
 }
